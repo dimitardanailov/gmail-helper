14e785c9c30af50b1a3d7f9b4755ffb40986313b
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-12-13 16:46:39 +0200

    Repo has configuration which are using lernajs

--- develop/test.ts
+++ develop/test.ts
@@ -4,5 +4,6 @@ import { listLabels, createLabel } from './api/gmail/label'
 (async () => {
 	const oAuth2Client = await GmailAuth.authorize()
 	const gmailLabels = await listLabels(oAuth2Client)	
+	console.log(gmailLabels)
 	// createLabel(oAuth2Client)
 })()
