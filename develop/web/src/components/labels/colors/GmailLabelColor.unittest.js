7e31bb143d1dddde792bac38cb89d03f9098f4b9
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-01-21 13:31:21 +0200

    GmailLabelColor has a test unit test.

---
+++ develop/web/src/components/labels/colors/GmailLabelColor.unittest.js
@@ -0,0 +1,51 @@
+import { expect } from 'chai'
+import { GmailLabelColor } from './GmailLabelColor'
+
+const color = '#000000'
+
+describe('gmail-label-color', () => {
+  // Elements is responsible to hold the testing area of this unit
+  let container
+
+  // Element will hold the target custom web component
+  let scratch
+
+  // This will hold the actual element under tests
+  let radio
+
+  before(done => {
+    container = testingHelper.before()
+
+    done()
+  })
+
+  beforeEach(done => {
+    scratch = document.createElement('div')
+    container.appendChild(scratch)
+
+    done()
+  })
+
+  beforeEach(async () => {
+    scratch.appendChild(new GmailLabelColor(color))
+    await testingHelper.waitForElement('gmail-label-color')
+    radio = scratch.querySelector('gmail-label-color')
+  })
+
+  afterEach(done => {
+    scratch = null
+
+    done()
+  })
+
+  after(done => {
+    container = testingHelper.after(container)
+    scratch = null
+
+    done()
+  })
+
+  it('should add a [role] to the radio', () => {
+    expect(radio.getAttribute('role')).to.equal('radio')
+  })
+})
