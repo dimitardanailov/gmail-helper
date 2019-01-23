337f026e6cfc597fadb83c644e83ab820d918b1d
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-01-23 14:12:09 +0200

    GmailLabelTextBox has a test suite.

---
+++ develop/web/src/components/text-fields/GmailLabelTextBox.unittest.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-expressions */
+import {
+  fixture,
+  expect,
+} from '@open-wc/testing';
+
+import { GmailLabelTextBox } from './GmailLabelTextBox'
+
+describe('gmail-label-text-box', () => {
+  it('gmail-label-text-box and child should be define', async () => {
+    const el = await fixture('<gmail-label-text-box></gmail-label-text-box>') 
+    expect(el instanceof GmailLabelTextBox).to.be.true
+  })
+})
