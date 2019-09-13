ff1a9558dd175e2cf1f1e08a91fbea1e08175151
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-09-13 23:40:13 +0300

    Click listener doesn't work with mwc-icon-button

--- develop/web/src/components/headers/AppBar.js
+++ develop/web/src/components/headers/AppBar.js
@@ -1,20 +1,26 @@
-import { LitElement, html } from 'lit-element'
+import { LitElement, html } from "lit-element";
 
-import '@material/mwc-top-app-bar'
-import '@material/mwc-icon-button'
+import "@material/mwc-top-app-bar";
+import "@material/mwc-icon-button";
 
 export class AppBar extends LitElement {
-
   render() {
     return html`
       <mwc-top-app-bar>
         <mwc-icon-button icon="menu" slot="navigationIcon"></mwc-icon-button>
         <div slot="title" id="title">Standard</div>
-        <mwc-icon-button icon="exit_to_app" slot="actionItems"></mwc-icon-button>
+        <mwc-icon-button
+          icon="exit_to_app"
+          slot="actionItems"
+          @click="${this.handleClick}"
+        ></mwc-icon-button>
       </mwc-top-app-bar>
-    `
+    `;
   }
 
+  handleClick(e) {
+    console.log('click')
+  }
 }
 
-customElements.define('gh-app-bar', AppBar)
+customElements.define("gh-app-bar", AppBar);
